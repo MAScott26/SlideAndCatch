47f01c861d2fbd5efe1ef484c1aabdad21c3fca9
MAScott26 <michael.scott@bsu.edu>
2024-03-29 10:48:50 -0400

    added coin image and mechanics

--- SlideAndCatch.py
+++ SlideAndCatch.py
@@ -4,11 +4,8 @@ SlideAndCatch
 
 @author: michael.scott
 """
-import random
-import simpleGE
-
-
-        
+import random, simpleGE, pygame
+  
 class Player(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -16,13 +13,39 @@ class Player(simpleGE.Sprite):
         self.sprites = [self]
         self.setSize(50, 50)
         self.position = (320, 400)
+        self.movespeed = 5
+        
+    def process(self):
+        if self.isKeyPressed(pygame.K_LEFT):
+            self.x -= self.movespeed 
+        if self.isKeyPressed(pygame.K_RIGHT):
+            self.x += self.movespeed
+
+class Coin(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("coin.png")
+        self.sprites = [self]
+        self.setSize(25, 25)
+        self.reset()
+        
+    def reset(self):
+        self.x = random.randint(0, self.screenWidth)
+        self.y = 10
+        self.dy = random.randint(3, 8)
+        
+    def checkBounds(self):
+        if self.bottom > self.screenHeight:
+            self.reset()
+
         
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.setImage("Background.png")
         self.player = Player(self)
-        self.sprites = [self.player]    
+        self.coin = Coin(self)
+        self.sprites = [self.player, self.coin]    
         
 def main():
     game = Game()
